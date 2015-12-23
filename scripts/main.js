c73524194262904d09c4bb5a12e2ca21ecad3fea
Benjamin <benjamin.j.arias@gmail.com>
2015-12-23 19:38:32 +0100

    feat: Created AddFishForm

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
@@ -24,6 +25,31 @@ var App = React.createClass({
 				<Order/>
 				<Inventory/>
 			</div>
+		)
+	}
+});
+
+/*
+
+	Add Fish Form
+	<AddFishForm />
+
+*/
+
+var AddFishForm = React.createClass({
+	render: function(){
+		return (
+			<form className="fish-edut" onSubmit={this.createFish}>
+				<input type="text" ref="name" placeholder="Fish Name"/>
+				<input type="text" ref="price" placeholder="Fish Price"/>
+				<select ref="status">
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea type="text" ref="desc" placeholder="Desc"></textarea>
+				<input type="text" ref="image" placeholder="URL to Image"/>
+				<button type="submit">+ Add Item</button>
+			</form>
 		)
 	}
 });
@@ -75,7 +101,10 @@ var Order = React.createClass({
 var Inventory = React.createClass({
 	render: function(){
 		return (
-			<p>Inventory</p>
+			<div>
+				<h2>Inventory</h2>
+				<AddFishForm/>
+			</div>
 		)
 	}
 
@@ -89,13 +118,13 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
-
-	goToStore: function(event){
+	mixins:[History],
+	goToStore: function(event) {
 		event.preventDefault();
 		//get the data from the input
 		var storeId = this.refs.storeId.value;
-		console.log(storeId);
 		//transition fron <StorePicker/> to <App/>
+		this.history.pushState(null, '/store/'+ storeId);
 	},
 	render: function() {
 		// Normal comments
@@ -103,7 +132,7 @@ var StorePicker = React.createClass({
 			<form className = "store-selector" onSubmit={this.goToStore}>
 				{/* Comments go here!*/}
 				<h2>Please enter a Store</h2>
-				<input type="text" ref="storedId" defaultValue ={h.getFunName()} required/>
+				<input type="text" ref="storeId" defaultValue ={h.getFunName()} required/>
 				<input type="submit"/>
 			</form>
 		)
