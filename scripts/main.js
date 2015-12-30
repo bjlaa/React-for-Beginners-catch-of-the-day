8027ba2e8d5ca929385d1b1822316caee5338ecb
Benjamin <benjamin.j.arias@gmail.com>
2015-12-30 18:16:34 +0100

    feat: Completed add to order

--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,7 @@ var App = React.createClass({
 	},
 	addToOrder: function(key) {
 		this.state.order[key] = this.state.order[key] + 1 || 1;
-		thi.setState({order: this.state.order});
+		this.setState({order: this.state.order});
 	},
 
 	addFish: function(fish){
@@ -67,7 +67,7 @@ var App = React.createClass({
 var Fish = React.createClass({
 	onButtonClick: function() {
 		console.log("Going to add the fish: ", this.props.index);
-		var key this.props.index;
+		var key = this.props.index;
 		this.props.addToOrder(key);
 
 	},
@@ -162,12 +162,43 @@ var Header = React.createClass({
 
 */
 var Order = React.createClass({
+	renderOrder : function(key) {
+		var fish = this.props.fishes[key];
+		var count = this.props.order[key];
+
+		if(!fish){
+			return <li key={key}>Sorry, fish no longer available!</li>
+		}
+		return (<li>
+			<span>{count}</span>lbs
+			{fish.name}
+			<span className="price">{h.formatPrice(count * fish.price)}</span>
+		</li>)
+	},
+
 	render: function(){
+		var orderIds = Object.keys(this.props.order);
+		var total = orderIds.reduce((prevTotal, key)=> {
+			var fish = this.props.fishes[key];
+			var count = this.props.order[key];
+			var isAvailable = fish && fish.status === 'available';
+
+			if(fish && isAvailable) {
+				return prevTotal + (count * parseInt(fish.price) || 0);
+			}
+			return prevTotal;
+		}, 0);	
+
 		return (
 			<div className="order-wrap">
-				<h2 className="order-title"></h2>
+				<h2 className="order-title">Your Order</h2>
 				<ul className="order">
-					
+					{orderIds.map(this.renderOrder)}
+					<li className="total">
+						<strong>Total:</strong>
+						{h.formatPrice(total)} 
+					</li>
+
 				</ul>
 			</div>
 		)
