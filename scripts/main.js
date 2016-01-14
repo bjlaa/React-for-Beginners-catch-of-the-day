51dfa7cc90e7d35fcd2e029f74075788ab58c865
Benjamin <benjamin.j.arias@gmail.com>
2016-01-14 20:50:06 +0100

    feat: Completed LinkState

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,7 @@ var Catalyst = require('react-catalyst');
 
 */
 var App = React.createClass({
+	mixins: [Catalyst.LinkedStateMixin],
 	getInitialState : function(){
 		return {
 			fishes: {},
@@ -76,7 +77,7 @@ var App = React.createClass({
 					</ul>
 				</div>
 				<Order fishes={this.state.fishes} order={this.state.order}/>
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+				<Inventory linkState={this.linkState} fishes={this.state.fishes} addFish={this.addFish} loadSamples={this.loadSamples}/>
 			</div>
 		)
 	}
@@ -239,10 +240,29 @@ var Order = React.createClass({
 
 */
 var Inventory = React.createClass({
+	renderInventory: function(key){
+		var linkState = this.props.linkState;
+		return (
+			<div className="fish-edit" key={key}>
+				<input type="text" valueLink={linkState('fishes.'+key+'.name')}/>
+				<input type="text" valueLink={linkState('fishes.'+key+'.price')}/>
+				<select valueLink={linkState('fishes.'+key+'.status')}>
+					<option value="unavailable">Sold Out!</option>
+					<option value="available">Fresh!</option>
+				</select>
+				<textarea valueLink={linkState('fishes.'+key+'.desc')}></textarea>
+				<input valueLink={linkState('fishes.'+key+'.image')}/>
+				<button>Remove Fish</button>
+			</div>
+		)
+	},
 	render: function(){
 		return (
 			<div>
 				<h2>Inventory</h2>
+
+				{Object.keys(this.props.fishes).map(this.renderInventory)}
+
 				<AddFishForm {...this.props }/>
 				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 			</div>
